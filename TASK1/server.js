d79e3b3e89b4f863a93abf902c86a0dccccba114
Urmeet2k24 <156178722+Urmeet2k24@users.noreply.github.com>
2025-01-22 09:14:52 +0530

    task1

---
+++ TASK1/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+//p.use(express.json());
+//p.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+    const logDetails = {
+        timestamp: new Date().toISOString(),
+        ip: req.ip,
+        url: req.originalUrl,
+        protocol: req.protocol,
+        method: req.method,
+        hostname: req.hostname,
+    };
+
+    const logMessage = JSON.stringify(logDetails) + '\n';
+
+    const logFilePath = path.join(__dirname, 'requests.log');
+    fs.appendFile(logFilePath, logMessage, (err) => {
+        if (err) {
+            console.error('Failed to write to log file:', err);
+        }
+    });
+
+    next(); 
+});
+app.get('/', (req, res) => {
+    res.send('Welcome to the Express Logger!');
+});
+app.get('/about', (req, res) => {
+    res.send('This is the About page.');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
